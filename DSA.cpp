670492711e08ce1c5fc35bf72fc9138129c566cd
Himanshu Choudhary <38095683+Himanshuchoudhary18@users.noreply.github.com>
2025-01-02 21:16:01 +0530

    Update DSA.cpp

--- DSA.cpp
+++ DSA.cpp
@@ -209,14 +209,14 @@
 // int main() {
 //     int n;
 //     cin >> n;
-// 	for(int i = 1; i <= n; i++)
-// 	{
-// 	    for(int j = 1; j <= n - i + 1; j++)
-// 	    {
-// 	        cout<< "*";
-// 	    }
-// 	    cout << endl;
-// 	}
+// 	   for(int i = 1; i <= n; i++)
+// 	   {
+// 	       for(int j = 1; j <= n - i + 1; j++)
+// 	       {
+// 	           cout<< "*";
+// 	       }
+// 	       cout << endl;
+// 	   }
 //     return 0;
 // }
 
